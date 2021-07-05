56fc006398b8166765fc6b4f570ad990c65611be
nalo26 <nath.v26@gmail.com>
2021-07-05 15:30:16 +0200

    navbar

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template
 import json
-import urllib.parse
+
 app = Flask(__name__)
 
 achievements_data = json.loads(open('static/Achievements.json', 'r', encoding='utf-8').read())
@@ -16,16 +16,22 @@ def main():
 def achievements_name(cat_name):
     cat_name = format_string(cat_name)
     for i, d in enumerate(achievements_data):
-        if cat_name in d["alternates"]: return redirect(f'/achievements/{i}')
+        if cat_name in d["aliases"]: return redirect(f'/achievements/{i}')
     return redirect('/achievements/0')
 
 @app.route('/achievement/<int:cat_id>')
 def achievement(cat_id):
     return redirect(f"/achievements/{cat_id}")
 
+
+# ---------------------------------------------------------------------------------------
 @app.route('/achievements/<int:cat_id>')
 def achievements(cat_id):
-    return render_template('achievements.html', achievements=achievements_data)
+    if cat_id < 0 or cat_id >= len(achievements_data): return redirect('/achievements/0')
+    
+    return render_template('achievements.html', achievements=achievements_data, category=cat_id)
+# ---------------------------------------------------------------------------------------
+
 
 @app.errorhandler(401)
 @app.errorhandler(404)
