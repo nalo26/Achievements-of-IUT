926fb558d3167189c1c06506e4337edcfedfd4e8
nalo26 <nath.v26@gmail.com>
2021-08-06 13:25:05 +0200

    user managing

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, g
+from flask import Blueprint, redirect, request, g
 import functools
 
 from flask.templating import render_template
@@ -12,7 +12,7 @@ admin_id = None
 
 def init_config(config):
     global admin_id
-    admin_id = config['DiscordApp']['admin_discord_id']
+    admin_id = int(config['DiscordApp']['admin_discord_id'])
 
 def admin_required(view):
     @functools.wraps(view)
@@ -24,17 +24,25 @@ def admin_required(view):
 
     return wrapped_view
 
-@bp.route('/users')
+@bp.route('/users', methods=('GET', 'POST'))
 @admin_required
 def manage_users():
     base = get_db()
-    users = base.execute("SELECT * FROM user u JOIN discord_user d USING(id_user)").fetchall()
     
+    if request.method == 'POST':
+        for key, value in request.form.items():
+            k, u_id = key.split('-')
+            user = base.execute("SELECT firstname, lastname, year FROM discord_user where id_user = ?", (u_id,)).fetchone()
+            if str(user[k]) == str(value): continue
+            base.execute(f"UPDATE discord_user SET {k} = ? WHERE id_user = ?", (value, u_id,))
+        base.commit()
+        
+    users = base.execute("SELECT * FROM user u JOIN discord_user d USING(id_user)").fetchall()
     return render_template("admin/users_manage.html", users=users)
 
 @bp.route('/achievements')
 @admin_required
-def manage_users():
+def manage_achievements():
     base = get_db()
     achievements = base.execute("SELECT * FROM achievement").fetchall()
     
