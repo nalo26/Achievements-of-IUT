42821559166f6cd8d4aea7f814b489c0ef8bebb2
nalo26 <nath.v26@gmail.com>
2021-08-07 12:54:48 +0200

    achievement managing now working

--- admin.py
+++ admin.py
@@ -44,10 +44,28 @@ def manage_users():
     users = base.execute("SELECT * FROM user u JOIN discord_user d USING(id_user)").fetchall()
     return render_template("admin/users_manage.html", users=users, admin_id=admin_id)
 
-@bp.route('/achievements')
+@bp.route('/achievements', methods=('GET', 'POST'))
 @admin_required
 def manage_achievements():
     base = get_db()
-    achievements = base.execute("SELECT * FROM achievement").fetchall()
     
+    if request.method == 'POST':
+        last_a_id = ""
+        for key, value in request.form.items():
+            k, a_id = key.split('-')
+            if k not in ('name', 'lore', 'difficulty'): continue
+            if a_id != last_a_id:
+                ach = base.execute("SELECT name, lore, difficulty FROM achievement where id_achievement = ?", (a_id,)).fetchone()
+            last_a_id = a_id
+            if str(ach[k]) == str(value): continue
+            base.execute(f"UPDATE achievement SET {k} = ? WHERE id_achievement = ?", (value, a_id,))
+            if k != 'difficulty':
+                base.execute(
+                    f"UPDATE user SET score = score-{ach['difficulty']}+{value} WHERE id_user in (" + \
+                    "SELECT id_user FROM done JOIN achievement USING(id_achievement) WHERE complete = 1 AND id_achievement = ?" + \
+                    ")", (a_id,)
+                )
+        base.commit()
+        
+    achievements = base.execute("SELECT * FROM achievement").fetchall()
     return render_template("admin/achievements_manage.html", achievements=achievements, admin_id=admin_id)
